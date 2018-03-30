bfb29822cc7c0eff25aa9eccd7bc2edeb9e8c301
Rey Espinosa <reynaldo.espinosa.III@gmail.com>
2018-03-30 16:24:08 -0500

    updated location of remove button and commented code to strike through

--- js/todo.js
+++ js/todo.js
@@ -4,16 +4,26 @@ console.log("Ready to go!");
 $(function() {
   $("#add").on("click", function(e) {
     e.preventDefault();
-    console.log("New Item Button Clicked");
+// Code below set new list item text as a variable
     let value = $("input").val();
+// Code below confirms the new list item field is not empty
     if (value !== "") {
+// Code below adds list html tag and includes text from input field
       let element = $("<li></li>").text(value);
-      $(element).append("<button>Remove</button>");
+// Code below add remove button after new li tag
+      $(element).prepend("<button>Remove</button>");
+// Code below add new item, li tag and remove button to ordered list
       $("#todolist").append(element);
+// Code below clears the new item input field
       $("#words").val("");
+// Code below removes list item and remove button
       $("button").on("click", function() {
-       $(this).closest('li').addClass("strike");
-        });
+        $(this).closest('li').remove();
+      })
+// Code below strikes through an item after remove button click
+      // $("button").on("click", function() {
+      //  $(this).closest('li').addClass("strike");
+      //   });
     }
   });
 });
